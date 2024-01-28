799314c4b9cfec5d94d138a591929c21a79a8e7f
老房东 <huangdong@gmail.com>
2024-01-28 10:02:29 -0500

    add all Scenes and they can switch

--- BigSad/Assets/Scripts/SceneSwitcher.cs
+++ BigSad/Assets/Scripts/SceneSwitcher.cs
@@ -5,20 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
